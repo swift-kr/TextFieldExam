c3dd040df207fecc52a647bf5ce792f8aff1c3c1
MJ Kim <myoungjun.kim@gmail.com>
2016-05-05 19:44:05 +0900

    first update

---
+++ TextFieldExam/UITextFieldExtension.swift
@@ -0,0 +1,25 @@
+import UIKit
+
+//--------------------------------------------------
+// TextField 입력박스 배경색 변경 확장
+//--------------------------------------------------
+@IBDesignable
+class UITextFieldExtension: UITextField, UITextFieldDelegate {
+
+    @IBInspectable var focusBackgroundColor: UIColor?
+    
+    private var defaultBackgroundColor: UIColor?
+    
+    func textFieldDidBeginEditing(textField: UITextField) {
+        if focusBackgroundColor == nil {
+            print("오류 - focusBackgroundCOlor 설정")
+            return
+        }
+        defaultBackgroundColor = self.backgroundColor
+        self.backgroundColor = focusBackgroundColor
+    }
+    
+    func textFieldDidEndEditing(textField: UITextField) {
+        self.backgroundColor = defaultBackgroundColor
+    }
+}
--- TextFieldExam/ViewController.swift
+++ TextFieldExam/ViewController.swift
@@ -1,25 +1,26 @@
-//
-//  ViewController.swift
-//  TextFieldExam
-//
-//  Created by MJ Kim on 2016. 5. 5..
-//  Copyright © 2016년 Swifter. All rights reserved.
-//
-
 import UIKit
 
+//--------------------------------------------------
+// TextField 확장 테스트 뷰컨트롤러
+//--------------------------------------------------
 class ViewController: UIViewController {
 
+    @IBOutlet weak var txtField1: UITextFieldExtension!
+    @IBOutlet weak var txtField2: UITextFieldExtension!
+    @IBOutlet weak var btnRun: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // 텍스트필드 커서에 따라 배경색 변경 처리
+        txtField1.delegate = txtField1
+        txtField2.delegate = txtField2
+
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
 
 }
 
